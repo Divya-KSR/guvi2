32f7d595b91d2ca6513b9febbb54ec49699ef0ea
Divya-KSR <divyapalanisamyb4u@gmail.com>
2017-06-05 15:59:43 +0530

    Create Ex1.java

---
+++ Ex1.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+import java.util.*;
+
+import java.util.Scanner;
+
+class Ex1
+
+{
+
+public static void main(String args[])
+
+{
+
+Scanner sc=new Scanner(System.in);
+
+int a=sc.nextInt();
+
+if(a==0)
+
+{
+
+System.out.println("Zero");
+
+}
+
+else if(a>0)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.println("negative");
+}
+}
+}
